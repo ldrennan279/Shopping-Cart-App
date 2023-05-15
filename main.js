bfa4f7c3ed9469bb800aec4136b57f57e502c59a
ldrennan279 <leedrennan279@gmail.com>
2023-05-15 06:39:48 +0100

    3rd commit
    Setting up firebase database with google & writing to database.

--- main.js
+++ main.js
@@ -1,32 +1,23 @@
 // Firebase setup
 import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import {getDatabase, ref , push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseURL:"https://realtime-database-31f3f-default-rtdb.europe-west1.firebasedatabase.app/"
+    databaseURL: "https://playground-8bc52-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shoppingListInDB = ref(database, "shoppingList")
+const shoppingListInDB = ref(database, "shopplingList")
 
 
+// HTML Elememnt Selection
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
-const shoppingList = document.getElementById("shopping-list")
+const shoppingListEl = document.getElementById("shopping-list")
 
-function clearInputField(){
-    inputFieldEl.value = ""
-}
-function addHtmlToListInDom(item){
-    shoppingList.innerHTML += `
-    <li>${item}</li>
-` 
-}
-
-addButtonEl.addEventListener("click", function() {
+// eventlistners
+addButtonEl.addEventListener('click',()=>{
     let inputValue = inputFieldEl.value
-    push(shoppingListInDB, inputValue)
-    addHtmlToListInDom(inputValue)
-    clearInputField()
+    push(shoppingListInDB ,inputValue)
 })
